d21f27b61dc135ae608527574c9cab4940499dc3
Semih Akkoç <52207216+SemihAkkoc@users.noreply.github.com>
2021-12-07 18:33:53 +0300

    Update Lab08_SemihAkkoc.py

--- CS115/lab/lab8/Lab08_SemihAkkoc.py
+++ CS115/lab/lab8/Lab08_SemihAkkoc.py
@@ -5,7 +5,7 @@ def selection_sort(countries):
     for i in range(len(countries)):
         curr_min = countries[i]
         for country in countries[i:]:
-            if curr_min.getContinent() > country.getContinent():
+            if curr_min.getContinent() + curr_min.getCountry() > country.getContinent() + country.getCountry():
                 curr_min = country
         countries.remove(curr_min)
         countries.insert(i, curr_min)
@@ -13,8 +13,7 @@ def selection_sort(countries):
 
 def search_countries(countries, continent, index):
     global continent_countries
-    continent_countries = []
-    if continent == countries[index]:
+    if continent == countries[index].getContinent():
         continent_countries.append(countries[index])
     if index != 0:
         search_countries(countries, continent, index-1)
@@ -23,7 +22,6 @@ def search_countries(countries, continent, index):
 def read_countries(filename):
     global countries
     file = open(filename, 'r')
-    countries = []
     for line in file:
         country = line.split(',')
         country[-1] = country[-1].strip()
@@ -31,13 +29,14 @@ def read_countries(filename):
 
 
 continent_countries, countries = [], []
+
 read_countries('country.txt')
 continents = input('Enter continent to search: ')
 print('List of Countries in', continents)
 search_countries(countries, continents, len(countries)-1)
 for country in continent_countries:
     print('Country:', country.getCountry())
-    print('Average Life Expectancy:', country.calculate_average())
+    print(f'Average Life Expectancy: {country.calculate_average():1f}')
 
 new_country_info = [x.strip() for x in input('Enter county name, continent, life expectany for Men and life expectany for Women: ').split(',')]
 countries.append(Country(new_country_info[0], new_country_info[1], new_country_info[2], new_country_info[3]))
